0055d17e28fd20fe57cc00db048a9303d701b385
Kirsten Keels <keelsk@gmail.com>
2020-03-07 00:11:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,6 +20,10 @@ class Waiter
     end
   end
   
+  def new_meal
+    Meal.new(self, @name)
+  end
+  
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
